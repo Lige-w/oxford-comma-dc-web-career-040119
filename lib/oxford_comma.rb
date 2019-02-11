2f08c294e25c4865b16adb083414102b642c1d33
Lige <lige.williamson@gmail.com>
2019-02-11 14:13:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  when 3
+    end_of_phrase = array.pop
+    phrase = array.join(", ")
+    phrase << ", and #{end_of_phrase}"
 
 end
